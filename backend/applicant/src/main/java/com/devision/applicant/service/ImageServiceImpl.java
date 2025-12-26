077b0645407fb3c9698d3b8c5891d886341ae697
Nguyen Hoang Son <s3990627@rmit.edu.vn>
2025-12-26 16:16:58 +0700

    update MediaPortfolio CRUD
    sync with applicants document

--- backend/applicant/src/main/java/com/devision/applicant/service/ImageServiceImpl.java
+++ backend/applicant/src/main/java/com/devision/applicant/service/ImageServiceImpl.java
@@ -5,23 +5,30 @@ import com.cloudinary.Transformation;
 import com.cloudinary.utils.ObjectUtils;
 import com.devision.applicant.enums.MediaType;
 import com.devision.applicant.enums.Visibility;
+import com.devision.applicant.model.Applicant;
 import com.devision.applicant.model.MediaPortfolio;
+import com.devision.applicant.repository.ApplicantRepository;
 import com.devision.applicant.repository.MediaPortfolioRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Map;
 
 @Service
 public class ImageServiceImpl implements ImageService{
     private final Cloudinary cloudinary;
     private final MediaPortfolioRepository mediaRepo;
+    private final ApplicantRepository applicantRepo;
 
-    public ImageServiceImpl(Cloudinary cloudinary, MediaPortfolioRepository mediaRepo){
+    public ImageServiceImpl(Cloudinary cloudinary, MediaPortfolioRepository mediaRepo, ApplicantRepository applicantRepo){
         this.cloudinary = cloudinary;
         this.mediaRepo = mediaRepo;
+        this.applicantRepo = applicantRepo;
     }
 
     @Override
@@ -58,7 +65,21 @@ public class ImageServiceImpl implements ImageService{
                 .visibility(visibility)
                 .createdAt(Instant.now())
                 .build();
-        return mediaRepo.save(media);
+
+        MediaPortfolio savedMedia = mediaRepo.save(media);
+
+        Applicant a = applicantRepo.findById(applicantId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Applicant not found"));
+
+        if(a.getMediaPortfolios() == null){
+            a.setMediaPortfolios(new ArrayList<>());
+        }
+
+        //Add the new media
+        a.getMediaPortfolios().add(savedMedia);
+
+        applicantRepo.save(a);
+        return savedMedia;
     }
 
     public MediaType determineMediaType(String resourceType) {
@@ -77,6 +98,15 @@ public class ImageServiceImpl implements ImageService{
         cloudinary.uploader().destroy(media.getPublicId(), ObjectUtils.emptyMap());
 
         // Delete from DB
+        Applicant a = applicantRepo.findById(applicantId)
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Can't find applicant"));
+
+        if(a.getMediaPortfolios() != null){
+            a.getMediaPortfolios().remove(media);
+            applicantRepo.save(a);
+        }
         mediaRepo.delete(media);
+
+
     }
 }
